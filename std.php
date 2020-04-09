3d0ec29a5bc9e3be5c8ce01e0d126e9609596fd5
VinaySingh108 <49630324+VinaySingh108@users.noreply.github.com>
2020-04-09 22:39:37 +0530

    Update std.php

--- std.php
+++ std.php
@@ -1,7 +1,99 @@
+
 <html>
-<head><title>Percentage Calculator of Marks</title></head>
+<head>
+
+<body style='background-color:gray'>
+
+<script> 
+function val()                                    
+{ 
+    var regno = document.forms["form1"]["reg"];               
+    var studentname = document.forms["form1"]["stname"];    
+    var opensource = document.forms["form1"]["ost"];  
+    var cryptography =  document.forms["form1"]["crypto"];  
+    var objectoriented = document.forms["form1"]["oops"];  
+    var python = document.forms["form1"]["py"];  
+   
+    if (regno.value == "")                                  
+    { 
+        window.alert("Please enter registration number."); 
+        regno.focus(); 
+        return false; 
+    } 
+   
+    if (studentname.value == "")                               
+    { 
+        window.alert("Please enter studentname."); 
+        studentname.focus(); 
+        return false; 
+    } 
+       
+    if (opensource.value == "")                                   
+    { 
+        window.alert("Please enter a marks."); 
+        opensource.focus(); 
+        return false; 
+    } 
+   
+    if (cryptography.value == "")                           
+    { 
+        window.alert("Please enter marks."); 
+        cryptography.focus(); 
+        return false; 
+    } 
+   
+    if (objectoriented.value == "")                        
+    { 
+        window.alert("Please enter marks."); 
+        objectoriented.focus(); 
+        return false; 
+    } 
+   
+    if (python.value == "")                  
+    { 
+        window.alert("Please enter marks."); 
+        python.focus(); 
+        return false; 
+    } 
+   
+    return true; 
+}</script>
+
+<style>
+
+val {                                         
+    margin-left: 70px; 
+    font-weight: bold ; 
+    float: left; 
+    clear: left; 
+    width: 100px; 
+    text-align: left; 
+    margin-right: 10px; 
+    font-family:sans-serif,bold, Arial, Helvetica; 
+    font-size:14px; 
+} 
+   
+div {  
+    box-sizing: border-box; 
+    width: 100%; 
+    border: 100px solid black; 
+    float: left; 
+    align-content: center; 
+    align-items: center; 
+} 
+   
+form {                                         
+    margin: 0 auto; 
+    width: 600px; 
+}</style>
+
+<title>Percentage Calculator of Marks</title></head>
+
 <body>
-<form id="form1" name="form1" method="post" action="Insert.php">
+<form id="form1" name="form1" method="post" onsubmit="return val()" action="Insert.php">
+
+ <h2 align="center" style='color:red'>Student Marks Update</h2>
+
   <table width="294" heigth="172" border="1" cellpadding="10" cellspacing="10" align="center">
   	<tr>
   		<td width="123">Reg. NO.</td>
